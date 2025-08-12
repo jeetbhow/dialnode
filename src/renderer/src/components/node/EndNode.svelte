3d8825e1a660819886aa51293e3d4ce3fb96ef1e
Jeet Bhowmik <jeetbhowm@gmail.com>
2025-08-11 17:08:00 -0700

    fix(graph editor): Add delete buttons to start and end nodes

--- src/renderer/src/components/node/EndNode.svelte
+++ src/renderer/src/components/node/EndNode.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
-  import { Handle, Position, type NodeProps } from "@xyflow/svelte";
+  import Cross from "../icons/Cross.svelte";
+  import { Handle, Position, useSvelteFlow, type NodeProps } from "@xyflow/svelte";
 
+  const ICON_SIZE = 24;
   const DEFAULT_HANDLE_STYLE = "width: 0.5rem; height: 0.5rem";
 
+  const { deleteElements } = useSvelteFlow();
+
   let { id }: NodeProps = $props();
+
+  function deleteThisNode() {
+    deleteElements({ nodes: [{ id }] });
+  }
 </script>
 
 <Handle
@@ -11,12 +19,23 @@
   position={Position.Left}
   style={`${DEFAULT_HANDLE_STYLE}; background-color: var(--target-handle-color);`}
 />
-<div {id} class="container">End</div>
+<div {id} class="container">
+  <p>End</p>
+  <button onclick={deleteThisNode}>
+    <Cross width={ICON_SIZE} height={ICON_SIZE} />
+  </button>
+</div>
 
 <style>
-  div {
-    font-size: 1.3rem;
-    font-weight: 600;
-    padding: 2rem 3rem;
+  .container {
+    align-items: center;
+    display: flex;
+    padding-inline: 1.2rem;
+    gap: 1rem;
+  }
+
+  button {
+    display: flex;
+    align-items: center;
   }
 </style>
